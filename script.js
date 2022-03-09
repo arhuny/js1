cd4e21a32631ed19668b26e0275c47ef1ed4df98
arhuny <91968435+arhuny@users.noreply.github.com>
2022-03-09 14:25:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+console.log("Hello World")
+console.log("Skill Safari")
+console.log(45)
+console.log(4.56)
+console.log(true)
+var nam = "Admin"
+var age = 21
+var college = "ABC College"
+console.log(nam + " is a student")
+console.log("Age is " + age)
+console.log("Studies in " + college)
+
+var q = 45
+console.log(q)
+
+var q = 100
+console.log(q)
+
+let a = 12
+console.log(a)
+
+//let a = 23
+//console.log(a)
+
+let b 
+b = "Skill Safari"
+console.log(b)
+
+// const
+var c = 15
+var d = 15
+
+const e = c + d
+
+console.log(e)
+
+//Area of circle
+var radius = 32
+var circle = Math.PI * radius * radius
+console.log("Area of circle: " + circle)
+
+//Circumference of a circle
+var radius = 12
+var circum = 2 * Math.PI * radius
+console.log("Circumference of circle: ",circum)
+
+//Radius of circle
+var rad = circum/(2 * Math.PI)
+console.log("Radius of circle: ",rad)
+
+//Error
+//const t
+//t = 100
+//console.log(t)
